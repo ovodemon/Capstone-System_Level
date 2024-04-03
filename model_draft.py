0942efad4de5eff79a5db99b49b4307fee985804
ovodemon <98861497+ovodemon@users.noreply.github.com>
2024-04-03 13:21:15 -0400

    Update model_draft.py

--- model_draft.py
+++ model_draft.py
@@ -11,18 +11,50 @@ import jax.example_libraries.optimizers as opt
 import jax.numpy as npj
 import matplotlib.pyplot as plt  # plotting
 import sax
+import time
 from tqdm.notebook import trange
 
 
-def coupling_efficiency(theta, eta_0=0.3, theta_1=10, dtheta=2.5):
+
+'Coupler'
+def effective_index(D=0.5, n_1=1.44, n_2=3.48):
+    n_eff = D*n_2 + (1-D)*n_1
+    return n_eff
+
+def coupling_angle(wlen, pitch, D=0.5, n_1=1.44, n_2=3.48):
+    n_eff = effective_index(D, n_1, n_2)
+    theta_1 = npj.arcsin((pitch*n_eff - wlen)/n_1)*180/npj.pi
+    return theta_1
+
+def eta_naught(length, eta_max=0.5, beta=22):
+    eta_0 = eta_max*(1-npj.exp(-length/beta))
+    return eta_0
+
+def angular_acceptance(length, dtheta_max=6.25, beta=22):
+    dtheta = dtheta_max*npj.exp(-length/beta)
+    return dtheta
+
+def coupling_efficiency(theta, wlen=1.55, length=15, pitch=0.85, D=0.5, n_1=1.44, n_2=3.48, eta_max=0.5, dtheta_max=6.25, beta=22):
+    theta_1 = coupling_angle(wlen, pitch, D, n_1, n_2)
+    eta_0 = eta_naught(length, eta_max, beta)
+    dtheta = angular_acceptance(length, dtheta_max, beta)
     eta = eta_0*npj.exp(-npj.square(theta-theta_1)*npj.log(2)/npj.square(dtheta))
-    return eta
+    return eta, theta_1
 
+def coupler(theta=10) -> sax.SDict:
+    eta, theta_1 = coupling_efficiency(theta)
+    coupler_dict = sax.reciprocal(
+        {
+            ("in0", "out0"): eta,
+        }
+    )
+    return coupler_dict
 
+'Taper'
 def taper(a=0.4, b=0.5, c=7, z=0.9, wavelen=1.55, n_eff=3.0, length=0.3, alpha=0.0):
     X = a*(b*npj.square(z) + (1-b)*z) + (1-a)*npj.square(npj.sin(c*z*npj.pi/2))
     phase = 2*npj.pi*length*n_eff/wavelen
-    transmission = X*npj.exp(1.j*phase)
+    transmission = (-X)*npj.exp(-1.j*phase)
     sdict = sax.reciprocal(
         {
             ("in0", "out0"): transmission
@@ -30,13 +62,8 @@ def taper(a=0.4, b=0.5, c=7, z=0.9, wavelen=1.55, n_eff=3.0, length=0.3, alpha=0
     )
     return sdict
 
-
-
-
-
-
-
-def waveguide(wavelen=1.55, n_eff=3.0, length=1, alpha=0.0):
+'90 degree bend'
+def ninetybend(wavelen=1.55, n_eff=3.0, length=0.5, alpha=0.0):
     """A simple mode of a waveguide with phase change and loss. All lengthscale units
     are in microns."""
     phase = 2*npj.pi*length*n_eff/wavelen
@@ -48,104 +75,31 @@ def waveguide(wavelen=1.55, n_eff=3.0, length=1, alpha=0.0):
     )
     return sdict
 
+'S bend'
+def sbend(wavelen=1.55, n_eff=3.0, length=1, alpha=0.0):
+    """A simple mode of a waveguide with phase change and loss. All lengthscale units
+    are in microns."""
+    phase = 2*npj.pi*length*n_eff/wavelen
+    transmission = npj.power(10, -(alpha * length)/20) * npj.exp(1.j*phase)
+    sdict = sax.reciprocal(
+        {
+            ("in0", "out0"): transmission
+        }
+    )
+    return sdict
 
-
-def  power_combiner(ampl_1=0.2, ampl_2=0.03, phase_1=1, phase_2=1, wavelen=1.55, length=0.5, n_eff=3.0):
-    ampl = npj.sqrt(npj.square(ampl_1) + npj.square(ampl_2) + 2*ampl_1*ampl_2*npj.exp(1.j*(phase_1-phase_2)))
-    phase_combine = npj.arctan((ampl_1*npj.sin(phase_1)+ampl_2*npj.sin(phase_2)) / (ampl_1*npj.cos(phase_1)+ampl_2*npj.cos(phase_2)))
+'Power Combiner: need determine in1 and in2'
+def  powercombine(ampl_1=0.2, ampl_2=0.1, phace_1=1, phace_2=1, wavelen=1.55, length=0.5, n_eff=3.0):
+    ampl = npj.sqrt(npj.square(ampl_1) + npj.square(ampl_2) + 2*ampl_1*ampl_2*npj.exp(1.j*(phace_1-phace_2)))
+    phase_combine = npj.arctan((ampl_1*npj.sin(phace_1)+ampl_2*npj.sin(phace_2)) / (ampl_1*npj.cos(phace_1)+ampl_2*npj.cos(phace_2)))
     phase_length = 2*npj.pi*length*n_eff/wavelen
     power_out = npj.square(ampl * npj.exp(1.j*(phase_combine+phase_length)))
     sdict = sax.reciprocal(
         {
-            ("in0", "out0"): power_out/(npj.square(ampl_1)),
-            ("in1", "out0"): power_out/(npj.square(ampl_2))
+            ("in0", "out0"): npj.sqrt(power_out/(npj.square(ampl_1))),
+            ("in1", "out0"): npj.sqrt(power_out/(npj.square(ampl_2))),
         }
         )
     return sdict
-
-'4 folded grating array'
-_4_folded_array, info = sax.circuit(
-    netlist={
-        "instances": {
-            "first_coupler": "coupler",
-            "first_taper": "taper",
-            "first_ninetybend": "ninetybend",
-            "first_sbend": "sbend",
-            "second_coupler": "coupler",
-            "second_taper": "taper",
-            "second_ninetybend": "ninetybend",
-            "second_sbend": "sbend",
-            "third_coupler": "coupler",
-            "third_taper": "taper",
-            "third_ninetybend": "ninetybend",
-            "third_sbend": "sbend",
-            "fourth_coupler": "coupler",
-            "fourth_taper": "taper",
-            "fourth_ninetybend": "ninetybend",
-            "fourth_sbend": "sbend",
-            "powercombine1": "powercombine",
-            "pc1_sbend": "sbend",
-            "powercombine2": "powercombine",
-            "pc2_sbend": "sbend",
-            "powercombine3": "powercombine",
-        },
-        "connections": {
-            "first_coupler,out0": "first_taper,in0",
-            "first_taper,out0": "first_ninetybend,in0",
-            "first_ninetybend,out0": "first_sbend,in0",
-            "second_coupler,out0": "second_taper,in0",
-            "second_taper,out0": "second_ninetybend,in0",
-            "second_ninetybend,out0": "second_sbend,in0",
-            "first_sbend,out0": "powercombine1,in0",
-            "second_sbend,out0": "powercombine1,in1",
-            "powercombine1,out0": "pc1_sbend,in0",
-            "third_coupler,out0": "third_taper,in0",
-            "third_taper,out0": "third_ninetybend,in0",
-            "third_ninetybend,out0": "third_sbend,in0",
-            "third_sbend,out0": "powercombine2,in0",
-            "pc1_sbend,out0": "powercombine2,in1",
-            "powercombine2,out0": "pc2_sbend,in0",
-            "fourth_coupler,out0": "fourth_taper,in0",
-            "fourth_taper,out0": "fourth_ninetybend,in0",
-            "fourth_ninetybend,out0": "fourth_sbend,in0",
-            "fourth_sbend,out0": "powercombine3,in0",
-            "pc2_sbend,out0": "powercombine3,in1",
-        },
-        "ports": {
-            "in0": "first_coupler,in0",
-            "in1": "second_coupler,in0",
-            "in2": "third_coupler,in0",
-            "in3": "fourth_coupler,in0",
-            "out0": "powercombine3,out0",
-        },
-    },
-    models={
-        "coupler": coupler,
-        "taper": taper,
-        "ninetybend": ninetybend,
-        "sbend": sbend,
-        "powercombine": powercombine,
-    },
-)
-
-
-
-theta = npj.linspace(0, 45, 100)
-
-mzi_array_theta = _4_folded_array(
-                        first_coupler={"theta": theta},
-                        second_coupler={"theta": theta},
-                        third_coupler={"theta": theta},
-                        fourth_coupler={"theta": theta})
-mzi_array_S11 = npj.abs(mzi_array_theta[('in0', 'out0')])
-mzi_array_S12 = npj.abs(mzi_array_theta[('in3', 'out0')])
-
-fig, ax1 = plt.subplots(1)
-ax2 = ax1.twinx()
-ax1.plot(theta, mzi_array_S11, color="blue", label="S11")
-ax2.plot(theta, mzi_array_S12, color="red", label="S13")
-plt.legend()
-plt.show()
-
 
 
